ffbb884d9826b5117456164c7db0ad723cc85e9e
ayagaa <odwar235@gmail.com>
2020-07-31 19:49:59 +0300

    Added login and register forms

---
+++ src/components/transaction-confirm.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Confirmation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <h1>Confirmation</h1> );
+    }
+}
+ 
+export default Confirmation;
---
+++ src/components/transactions.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Transactions extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <h1>Transactions</h1> );
+    }
+}
+ 
+export default Transactions;
